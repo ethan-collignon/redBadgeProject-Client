5ed60522caae4a4abe06f4004bd5658a78eca7a4
Ethan Collignon <89707534+ethan-collignon@users.noreply.github.com>
2021-11-17 19:10:03 -0500

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+const App: React.FunctionComponent = () => {
+  return (
+    <div className="App">
+     <div className="stylehere">
+
+     </div>
+    </div>
+  );
+}
+
+export default App;
