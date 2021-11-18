7d935170382b1ed7d3441a969cd559510b9a7572
Ethan Collignon <89707534+ethan-collignon@users.noreply.github.com>
2021-11-18 11:37:17 -0500

    nav/routes added

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
+import Navigation from './site/Navbar';
 
 const App: React.FunctionComponent = () => {
   return (
     <div className="App">
      <div className="stylehere">
-
+       <Navigation/>
+      test
      </div>
     </div>
   );
